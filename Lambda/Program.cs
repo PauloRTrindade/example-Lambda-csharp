f65b9f027e9a9ce72dbe40a5016b77fee294b623
PauloRTrindade <prtrindadesf@gmail.com>
2023-06-11 19:01:53 -0300

    Created class 'Employee'

---
+++ Lambda/Program.cs
@@ -0,0 +1,11 @@
+﻿using Lambda.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Employee employee = new Employee("Paulo", "paulo@gmail.com", 5000.00);
+
+        Console.WriteLine(employee);
+    }
+}
